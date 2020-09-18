8c1757613bce1c4a05f15c8a5556c22e6f49028c
s-brekke <stein.brekke@eui.eu>
2020-09-18 13:19:02 +0200

    Delete CJEU files
    Delete CJEU data base files

--- systemdata/cjeu/compile.R
+++
@@ -1,7 +0,0 @@
-Decisions <- Decisions[,c("case", "celex", "ecli", "court", "type", "subject_matter", "date_document", "date_lodged",
-"subject_matter_subcategory", "preliminary_ruling", "direct_action", "applicant_actor", "defendant_actor")] 
-
-save(Decisions, file="Decisions.rda")
-
-Cases <- Cases[,c("case", "celex", "court", "status", "lodged_date", "title", "joined_cases", "appeal_of")]
-save(Cases, file="Cases.rda")
